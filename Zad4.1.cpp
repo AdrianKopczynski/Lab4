6c9ef8321a787d25cee716d10adc24124ec3a76e
AdrianKopczynski <115359523+AdrianKopczynski@users.noreply.github.com>
2022-12-17 15:42:42 +0100

    Add files via upload

---
+++ Zad4.1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int szukanie_najwiekszego(int tab[]){
+	
+}
+int main()
+{
+    int tableCap;
+    cout << "Podaj ilosc elementow tablicy: ";
+    cin >> tableCap;
+    int table[tableCap];
+    for (int i = 0; i<=tableCap;i++){
+    	int temp = 0;
+    	cout << "Podaj wartosc dla elementu " << i+1 << " : " <<endl;
+    	cin >> temp;
+    	cout << endl;
+    	table[i] = temp;
+	}
+	int max= 0;
+	for (int i = 0; i<=tableCap;i++){
+		if (table[i]>=max){
+			max = table[i];
+		}
+		else{
+			continue;
+		}
+	}
+	cout << "Najwieksza wartosc to " << max ;
+	
+    return 0;
+}
